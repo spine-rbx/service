e7c580b4b5005c63fe80da5b17db869b87040d50
Uncontained0 <uncontained0@gmail.com>
2022-07-21 01:55:46 -0500

    made networking easier and parallel execution possible

--- src/Server.lua
+++ src/Server.lua
@@ -5,31 +5,36 @@ local Object = require(Packages.object)
 local Net = require(Packages.net)
 local Util = require(Packages.util)
 
-local GlobalFolder = Util.Make("Folder", {
+local GlobalFolder = ReplicatedStorage:FindFirstChild("SpineServices") or Util.Make("Folder", {
 	Name = "SpineServices",
 	Parent = ReplicatedStorage,
 })
 
 local ServiceClient = Object:Extend()
 
-function ServiceClient:Constructor(Name: string, Server)
-	getmetatable(self).__newindex = self.__newindex
+local VALUE = newproxy()
+local SIGNAL = newproxy()
+local CALLBACK = newproxy()
 
+function ServiceClient:Constructor(Name: string, Server)
 	self._Name = Name
 	self._Server = Server
 	self._Hidden = {}
-	self.Signal = Net.Server.RemoteSignal
-	self.Callback = Net.Server.RemoteCallback
-	self.Value = Net.Server.RemoteValue
+
+	getmetatable(self).__newindex = self.__newindex
 end
 
 function ServiceClient:_Make()
-	self._Make = function() end
+	rawset(self, "_Make", function() end)
 
-	self._Instance = Util.Make("Folder", {
-		Name = self._Name,
-		Parent = GlobalFolder,
-	})
+	rawset(
+		self,
+		"_Instance",
+		Util.Make("Folder", {
+			Name = self._Name,
+			Parent = GlobalFolder,
+		})
+	)
 end
 
 function ServiceClient:__newindex(i, v)
@@ -37,12 +42,19 @@ function ServiceClient:__newindex(i, v)
 
 	if type(v) == "function" then
 		if self._Hidden[i] == nil then
-			self._Hidden[i] = self.Callback:New(self._Instance, i)
+			self._Hidden[i] = Net.Server.RemoteCallback:New(self._Instance, i)
 			self._Hidden[i].Callback = function(...)
-				v(self._Server, ...)
+				return v(self._Server, ...)
 			end
 		end
+	elseif v == SIGNAL then
+		v = Net.Server.RemoteSignal:New(self._Instance, i)
+	elseif v == CALLBACK then
+		v = Net.Server.RemoteCallback:New(self._Instance, i)
+	elseif v == VALUE then
+		v = Net.Server.RemoteValue:New(self._Instance, i)
 	elseif v == nil and self._Hidden[i] ~= nil then
+		self._Hidden[i]:Destroy()
 		self._Hidden[i] = nil
 	end
 
@@ -51,6 +63,10 @@ end
 
 local Service = Object:Extend()
 
+Service.VALUE = VALUE
+Service.SIGNAL = SIGNAL
+Service.CALLBACK = CALLBACK
+
 function Service:Constructor(Name: string)
 	self.Client = ServiceClient:New(Name, self)
 end
@@ -58,19 +74,19 @@ end
 export type ServiceClient = Object.Object<{
 	_Name: string,
 	_Server: Service,
-	_Hidden: {[string]: Net.ServerRemoteCallback},
+	_Hidden: { [string]: any },
 	_Instance: Instance,
-
-	Signal: Net.ServerRemoteSignal,
-	Callback: Net.ServerRemoteCallback,
-	Value: Net.ServerRemoteValue,
 
 	_Make: () -> (),
 	__newindex: (self: ServiceClient, i: string, v: any) -> (),
-}, (Name: string, Server: Service)>
+}, (string, Service)>
 
 export type Service = Object.Object<{
 	Client: ServiceClient,
-}, (Name: string)>
+
+	VALUE: any,
+	SIGNAL: any,
+	CALLBACK: any,
+}, (string)>
 
 return Service :: Service
