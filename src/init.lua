920bf65d75cfefc52fdf3a1f0f893912bd53951d
Uncontained0 <uncontained0@gmail.com>
2022-07-21 01:55:12 -0500

    added runservice logic

--- src/init.lua
+++ src/init.lua
@@ -1,4 +1,10 @@
+local RunService = game:GetService("RunService")
+
+local LOCKED_TABLE = setmetatable({}, {__index = function()
+	error("Attempt to access service across the client-server boundary.")
+end})
+
 return {
-	Server = require(script.Server),
-	Client = require(script.Client),
+	Server = if RunService:IsServer() and RunService:IsRunning() then require(script.Server) else LOCKED_TABLE,
+	Client = if not RunService:IsServer() and RunService:IsRunning() then require(script.Client) else LOCKED_TABLE,
 }
